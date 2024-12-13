1d1d049bcf67f80564b418b398bbc47387f64485
Daniel <zapatadanelstiven1710s.c@gmail.com>
2024-12-13 16:50:22 -0500

    Add files via upload

---
+++ Codigo/Repositories/Interfaces/ISatelite.cs
@@ -0,0 +1,15 @@
+﻿using proyectocountertexdefinitivo.Models;
+
+namespace proyectocountertexdefinitivo.Repositories.Interfaces
+{
+    public interface ISatelite
+    {
+
+        Task<List<Satelite>> GetSatelite();
+        Task<bool> PostSatelite(Satelite satelite);
+        Task<bool> PutSatelite(Satelite satelite);
+        Task<bool> DeleteSatelite( int id);
+
+    }
+
+}
---
+++ Codigo/Repositories/Interfaces/IUsuarios.cs
@@ -0,0 +1,13 @@
+﻿using proyectocountertexdefinitivo.Models;
+
+namespace proyectocountertexdefinitivo.Repositories.Interfaces
+{
+    public interface IUsuarios
+    {
+        Task<List<Usuarios>> GetUsuarios();
+        Task<bool> PostUsuarios(Usuarios usuarios);
+        Task<bool> PutUsuarios(Usuarios usuarios);
+     
+        Task<bool> DeleteUsuarios(int id);
+    }
+}
