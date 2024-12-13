5087097c42448cc6a7302ecd6abd03c2cc4e8dfe
Daniel <zapatadanelstiven1710s.c@gmail.com>
2024-12-13 14:41:29 -0500

    Add files via upload

---
+++ Code/Repositories/Interfaces/IAdministrador.cs
@@ -0,0 +1,15 @@
+﻿using proyectocountertexdefinitivo.Models;
+
+namespace proyectocountertexdefinitivo.Repositories.Interfaces
+{
+    public interface IAdministrador
+    {
+        
+        Task<List<PerfilAdministrador>> GetAdministrador();
+    
+        Task<bool> PostAdministrador(PerfilAdministrador perfilAdministrador);
+        Task<bool> PutAdministrador(PerfilAdministrador perfilAdministrador);
+        Task<bool> DeleteAdministrador(PerfilAdministrador perfilAdministrador);
+        Task<bool> DeleteAdministrador(int id);
+    }
+}
---
+++ Code/Repositories/Interfaces/IProvedor.cs
@@ -0,0 +1,14 @@
+﻿using proyectocountertexdefinitivo.Models;
+
+
+namespace proyectocountertexdefinitivo.Repositories.Interfaces
+{
+    public interface IProvedor
+    {
+        Task<List<Proveedor>> GetProveedor();
+        Task<bool> PostProveedor(Proveedor proveedor);
+        Task<bool> PutProveedor(Proveedor proveedor);
+        Task<bool> DeleteProveedor(Proveedor proveedor);
+        Task<bool> DeleteProveedor(int id);
+    }
+}
