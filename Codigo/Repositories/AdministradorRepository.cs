ac97f64b9cdb6b2582f503e906c1411b7151c934
Daniel <zapatadanelstiven1710s.c@gmail.com>
2024-12-13 16:49:42 -0500

    Add files via upload

---
+++ Codigo/Repositories/AdministradorRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyectocountertexdefinitivo.contexto;
+using proyectocountertexdefinitivo.Models;
+
+using proyectocountertexdefinitivo.Repositories.Interfaces;
+
+namespace proyectocountertexdefinitivo.Repositories.repositories
+{
+    public class AdministradorRepository :IAdministrador
+    {
+        private readonly CounterTexDBContext context;
+        public AdministradorRepository(CounterTexDBContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<PerfilAdministrador>> GetAdministrador()
+        {
+            var data = await context.PerfilAdministradores.ToListAsync();
+            return data;
+        }
+        public async Task<bool> PostAdministrador(PerfilAdministrador perfilAdministrador)
+        {
+            await context.PerfilAdministradores.AddAsync(perfilAdministrador);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> PutAdministrador(PerfilAdministrador perfilAdministrador)
+        {
+            context.PerfilAdministradores.Update(perfilAdministrador);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> DeleteAdministrador(PerfilAdministrador perfilAdministrador)
+        {
+            context.PerfilAdministradores.Remove(perfilAdministrador);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteAdministrador(int id)
+        {
+            var entity = await context.PerfilAdministradores.FindAsync(id);
+            if (entity == null) return false;
+
+            context.PerfilAdministradores.Remove(entity);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+    }
+}
---
+++ Codigo/Repositories/RegistroRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyectocountertexdefinitivo.contexto;
+using proyectocountertexdefinitivo.Models;
+using proyectocountertexdefinitivo.Repositories.Interfaces;
+
+namespace proyectocountertexdefinitivo.Repositories
+{
+    public class RegistroRepository : IRegistro
+    {
+        private readonly CounterTexDBContext _context;
+        public RegistroRepository(CounterTexDBContext context)
+        {
+            this._context = context;
+        }
+        public async Task<List<Registro>> GetRegistro()
+        {
+            var data = await _context.Registros.ToListAsync();
+            return data;
+        }
+        public async Task<bool> PostRegistro(Registro registro)
+        {
+            await _context.Registros.AddAsync(registro);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> PutRegistro(Registro registro)
+        {
+            _context.Registros.Update(registro);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> DeleteRegistro(int id)
+        {
+            var registro = await _context.Registros.FindAsync(id); // Usar 'context' en lugar de '_context'
+            if (registro == null) return false; // Si no existe, devolver 'false'
+
+            _context.Registros.Remove(registro); // Usar 'context'
+            await _context.SaveChangesAsync(); // Corregir 'SaveAsync' por 'SaveChangesAsync'
+            return true;
+        }
+
+
+    }
+}
