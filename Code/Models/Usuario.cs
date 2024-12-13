22e1577e41f3a0fa4c3f4734bf33f5c514ffcf5b
Daniel <zapatadanelstiven1710s.c@gmail.com>
2024-12-13 14:49:42 -0500

    Add files via upload

---
+++ Code/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using proyectocountertexdefinitivo.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace proyectocountertexdefinitivo.Models
+{
+    public class Usuarios
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }  
+    }
+}
