8d5c538549aeb41f3b3d680947e3d699acccd40a
Daniel <zapatadanelstiven1710s.c@gmail.com>
2024-12-13 16:47:17 -0500

    Add files via upload

---
+++ Codigo/Models/Registro.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace proyectocountertexdefinitivo.Models
+{
+    public class Registro
+    {
+        public int IdRegistro { get; set; }
+
+  
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Documento { get; set; } 
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+        public string ConfirmarContraseña { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+    }
+}
