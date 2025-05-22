44f801700553b0a0d02226aed1046d10ba30ec5a
AngelRamirez14 <angelyoloxds@gmail.com>
2025-05-22 15:06:59 -0500

    commit models

---
+++ Codigo/Models/AsignacionAdmin.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CounterTexAPI.Models
+{
+    public class Asignacion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string TipoPrenda { get; set; }
+
+        [Required]
+        public string Estado { get; set; }
+
+        [Required]
+        public string Prioridad { get; set; }
+
+        [Required]
+        public string AsignadoA { get; set; }
+
+        public string Comentarios { get; set; }
+    }
+}
---
+++ Codigo/Models/AsignacionEmpleado.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CounterTexAPI.Models
+{
+    public class AsignacionEmpleado
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Tipo { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public string Prioridad { get; set; }
+
+        [Required]
+        public string Estado { get; set; }
+
+        public int? OperadorId { get; set; }
+
+        [StringLength(500)]
+        public string Comentarios { get; set; }
+    }
+}
