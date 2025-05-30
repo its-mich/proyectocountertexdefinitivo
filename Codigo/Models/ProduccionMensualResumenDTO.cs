342f6ac0ddc090ae3702f76371cf2132849f5f8f
Joseasegura <joseavelinosegurabociga@gmail.com>
2025-05-30 00:55:52 -0500

    Se agrega Produccion Mensual

---
+++ Codigo/Models/ProduccionMensualResumenDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace proyectocountertexdefinitivo.Models
+{
+    public class ProduccionMensualResumenDTO
+    {
+        public int Mes { get; set; }
+        public int Anio { get; set; }
+        public decimal ProduccionTotalMensual { get; set; }
+    }
+}
